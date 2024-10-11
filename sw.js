497c85c1e1a0713ddd0fa9271d6e01740af31b92
Pamperkyumi <Pamperprms2024@gmail.com>
2024-10-11 22:04:43 +0800

    Site updated: 2024-10-11 22:04:42

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1728653361643";
+const VERSION = "1728655479413";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
