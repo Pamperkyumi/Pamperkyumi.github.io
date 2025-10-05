8a0e9d7916a00d002eaf48a1e0c185d2839db978
pamperkyumi <pamperprms2024@gmail.com>
2025-10-05 17:53:07 +0800

    Site updated: 2025-10-05 17:53:06

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759657849616";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759657970481";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
